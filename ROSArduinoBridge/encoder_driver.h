18143f6e43af3820cca674591c527f6530e981d0
basil <basilkhamala@gmail.com>
2024-12-01 14:12:09 +0300

    nothing

--- ROSArduinoBridge/encoder_driver.h
+++ ROSArduinoBridge/encoder_driver.h
@@ -3,7 +3,6 @@
    ************************************************************ */
    
    
-#ifdef ARDUINO_ENC_COUNTER
   //below can be changed, but should be PORTD pins; 
   //otherwise additional changes in the code are required
   #define LEFT_ENC_PIN_A PD2  //pin 2
@@ -12,7 +11,7 @@
   //below can be changed, but should be PORTC pins
   #define RIGHT_ENC_PIN_A PC4  //pin A4
   #define RIGHT_ENC_PIN_B PC5   //pin A5
-#endif
+
    
 long readEncoder(int i);
 void resetEncoder(int i);
