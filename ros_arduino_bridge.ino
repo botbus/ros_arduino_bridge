d19323ff44173aaf41bc1cd76eb23fb24427d07b
basil <basilkhamala@gmail.com>
2024-12-03 14:33:16 +0300

    small changes

--- ros_arduino_bridge.ino
+++ ros_arduino_bridge.ino
@@ -137,8 +137,8 @@ void motor_driver(void *pvParameters) {
   while (1) {
     if (run_right_ISR)
       RUN_PIN_ISR_RIGHT();
-//    if (run_left_ISR)
- //     RUN_PIN_ISR_LEFT();
+    if (run_left_ISR)
+     RUN_PIN_ISR_LEFT();
 
 //Serial.println("Reading motor driver");
     if (xSemaphoreTake(xSemaphore, (TickType_t)portMAX_DELAY) == pdTRUE) {
