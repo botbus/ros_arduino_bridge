ed1c85be27e94521a914ecb730611be0ee5ee89a
basil <basilkhamala@gmail.com>
2024-12-05 09:05:48 +0300

    small changes

--- encoder_driver.ino
+++ encoder_driver.ino
@@ -32,70 +32,70 @@ void initMotorPins()
   // attachInterrupt(digitalPinToInterrupt(LEFT_ENC_PIN_B), PIN_ISR_LEFT, CHANGE);
   // attachInterrupt(digitalPinToInterrupt(RIGHT_ENC_PIN_B), PIN_ISR_RIGHT, CHANGE);
 }
-void PIN_ISR_LEFT()
-{
-  run_left_ISR = true;
-}
-void PIN_ISR_RIGHT()
-{
-  run_right_ISR = true;
-}
+// void PIN_ISR_LEFT()
+// {
+//   run_left_ISR = true;
+// }
+// void PIN_ISR_RIGHT()
+// {
+//   run_right_ISR = true;
+// }
 
-void RUN_PIN_ISR_LEFT(void *pvParameters)
-{
-  (void)pvParameters;
-  xSemaphoreTake(xSemaphore, (TickType_t)portMAX_DELAY);
-  Serial.println("started LEFT ENC TASK");
-  xSemaphoreGive(xSemaphore);
-  while (1)
-  {
+// void RUN_PIN_ISR_LEFT(void *pvParameters)
+// {
+//   (void)pvParameters;
+//   xSemaphoreTake(xSemaphore, (TickType_t)portMAX_DELAY);
+//   Serial.println("started LEFT ENC TASK");
+//   xSemaphoreGive(xSemaphore);
+//   while (1)
+//   {
 
-    if (run_left_ISR)
-    {
-      run_left_ISR = false;
-      noInterrupts(); // Disable interrupts
-      if (xSemaphoreTake(xSemaphoreENC, (TickType_t)portMAX_DELAY) == pdTRUE)
-      {
-        long gpio_states = sio_hw->gpio_in;
-        static uint8_t enc_last = 0;
-        enc_last <<= 2;
-        enc_last |= ((gpio_states >> LEFT_ENC_PIN_A << 1) | (gpio_states >> LEFT_ENC_PIN_B));
-        // left_enc_pos += ENC_STATES[(enc_last & 0x0f)];
+//     if (run_left_ISR)
+//     {
+//       run_left_ISR = false;
+//       noInterrupts(); // Disable interrupts
+//       if (xSemaphoreTake(xSemaphoreENC, (TickType_t)portMAX_DELAY) == pdTRUE)
+//       {
+//         long gpio_states = sio_hw->gpio_in;
+//         static uint8_t enc_last = 0;
+//         enc_last <<= 2;
+//         enc_last |= ((gpio_states >> LEFT_ENC_PIN_A << 1) | (gpio_states >> LEFT_ENC_PIN_B));
+//         // left_enc_pos += ENC_STATES[(enc_last & 0x0f)];
 
-        interrupts();
-        xSemaphoreGive(xSemaphoreENC);
-      }
-    }
-    vTaskDelay(ENCDelay);
-  }
-}
-void RUN_PIN_ISR_RIGHT(void *pvParameters)
-{
-  (void)pvParameters;
-  xSemaphoreTake(xSemaphore, (TickType_t)portMAX_DELAY);
-  Serial.println("started RIGHT ENC TASK");
-  xSemaphoreGive(xSemaphore);
-  while (1)
-  {
-    if (run_right_ISR)
-    {
+//         interrupts();
+//         xSemaphoreGive(xSemaphoreENC);
+//       }
+//     }
+//     vTaskDelay(ENCDelay);
+//   }
+// }
+// void RUN_PIN_ISR_RIGHT(void *pvParameters)
+// {
+//   (void)pvParameters;
+//   xSemaphoreTake(xSemaphore, (TickType_t)portMAX_DELAY);
+//   Serial.println("started RIGHT ENC TASK");
+//   xSemaphoreGive(xSemaphore);
+//   while (1)
+//   {
+//     if (run_right_ISR)
+//     {
 
-      if (xSemaphoreTake(xSemaphoreENC, (TickType_t)portMAX_DELAY) == pdTRUE)
-      {
-        noInterrupts();
-        long gpio_states = sio_hw->gpio_in;
-        static uint8_t enc_last_right = 0;
-        enc_last_right <<= 2;
-        enc_last_right |= ((gpio_states >> RIGHT_ENC_PIN_B) << 1) | (gpio_states >> RIGHT_ENC_PIN_B);
-        // right_enc_pos += ENC_STATES[(enc_last_right & 0x0f)];
+//       if (xSemaphoreTake(xSemaphoreENC, (TickType_t)portMAX_DELAY) == pdTRUE)
+//       {
+//         noInterrupts();
+//         long gpio_states = sio_hw->gpio_in;
+//         static uint8_t enc_last_right = 0;
+//         enc_last_right <<= 2;
+//         enc_last_right |= ((gpio_states >> RIGHT_ENC_PIN_B) << 1) | (gpio_states >> RIGHT_ENC_PIN_B);
+//         // right_enc_pos += ENC_STATES[(enc_last_right & 0x0f)];
 
-        interrupts();
-        xSemaphoreGive(xSemaphoreENC);
-      }
-    }
-    vTaskDelay(ENCDelay);
-  }
-}
+//         interrupts();
+//         xSemaphoreGive(xSemaphoreENC);
+//       }
+//     }
+//     vTaskDelay(ENCDelay);
+//   }
+// }
 
 void right_ENC(void *pvParameters)
 {
@@ -106,53 +106,56 @@ void right_ENC(void *pvParameters)
   while (1)
   {
     // static int prevVal = (digitalRead(RIGHT_ENC_PIN_A) << 1) | digitalRead(RIGHT_ENC_PIN_B);
-    static int newVal{0};
-    int valA = digitalRead(RIGHT_ENC_PIN_A);
-    int valB = digitalRead(RIGHT_ENC_PIN_B);
-    static int prevVal = (valA << 1) + valB;
-    newVal = (valA << 1) + valB;
-    int info = ENC_STATES[prevVal][newVal];
-    static unsigned int clockState = 0;
-    static unsigned int counterClockState = 0;
+    static int newVal_right{0};
+    static unsigned int clockState_right = 0;
+    static unsigned int counterClockState_right = 0;
 
-    if (info == 1)
+    int valA_right = digitalRead(RIGHT_ENC_PIN_A);
+    int valB_right = digitalRead(RIGHT_ENC_PIN_B);
+
+    static int prevVal_right = (valA_right << 1) + valB_right;
+    newVal_right = (valA_right << 1) + valB_right;
+
+    int info_right = ENC_STATES[prevVal_right][newVal_right];
+
+    if (info_right == 1)
     {
-      clockState |= (1 << newVal); // set the bit to 1
+      clockState_right |= (1 << newVal_right); // set the bit to 1
     }
-    else if (info == -1)
+    else if (info_right == -1)
     {
-      counterClockState |= (1 << newVal);
+      counterClockState_right |= (1 << newVal_right);
     }
-    else if (info == 2)
+    else if (info_right == 2)
     {
       // Serial.println("skipped a value");
     }
 
-    if (prevVal != newVal && newVal == 3)
+    if (prevVal_right != newVal_right && newVal_right == 3)
     {
       // changed to the non moving state, lets figure out what direction we went!
 
       // for each clockwise and counterclockwise, the encoder state goes through 4 distinct states
       // make sure it's gone through at least 3 of those (and assume if one is missing it's because I didn't read fast enough)
-      if (clockState == 0b1011 || clockState == 0b1101 || clockState == 0b1110 || clockState == 0b1111)
+      if (clockState_right == 0b1011 || clockState_right == 0b1101 || clockState_right == 0b1110 || clockState_right == 0b1111)
       {
         xSemaphoreTake(xSemaphore, (TickType_t)portMAX_DELAY);
         Serial.println("RIGHT ++");
-         xSemaphoreGive(xSemaphore);
+        xSemaphoreGive(xSemaphore);
         right_enc_pos++;
       }
-      if (counterClockState == 0b1011 || counterClockState == 0b1101 || counterClockState == 0b1110 || counterClockState == 0b1111)
+      if (counterClockState_right == 0b1011 || counterClockState_right == 0b1101 || counterClockState_right == 0b1110 || counterClockState_right == 0b1111)
       {
         xSemaphoreTake(xSemaphore, (TickType_t)portMAX_DELAY);
         Serial.println("RIGHT --");
-         xSemaphoreGive(xSemaphore);
+        xSemaphoreGive(xSemaphore);
         right_enc_pos--;
       }
 
-      clockState = 0;
-      counterClockState = 0;
+      clockState_right = 0;
+      counterClockState_right = 0;
     }
-    prevVal = newVal;
+    prevVal_right = newVal_right;
     vTaskDelay(ENCDelay);
   }
 }
@@ -198,14 +201,14 @@ void left_ENC(void *pvParameters)
       {
         xSemaphoreTake(xSemaphore, (TickType_t)portMAX_DELAY);
         Serial.println("LEFT ++");
-         xSemaphoreGive(xSemaphore);
+        xSemaphoreGive(xSemaphore);
         left_enc_pos++;
       }
       if (counterClockState == 0b1011 || counterClockState == 0b1101 || counterClockState == 0b1110 || counterClockState == 0b1111)
       {
         xSemaphoreTake(xSemaphore, (TickType_t)portMAX_DELAY);
         Serial.println("LEFT --");
-         xSemaphoreGive(xSemaphore);
+        xSemaphoreGive(xSemaphore);
         left_enc_pos--;
       }
 
