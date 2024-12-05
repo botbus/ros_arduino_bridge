715cb380731302a62693fd27c413f3313fd7e754
basil <basilkhamala@gmail.com>
2024-12-05 08:24:55 +0300

    small changes

--- encoder_driver.ino
+++ encoder_driver.ino
@@ -103,9 +103,9 @@ void right_ENC()
   static int prevVal = (digitalRead(RIGHT_ENC_PIN_A) << 1) | digitalRead(RIGHT_ENC_PIN_B);
   int val = (digitalRead(RIGHT_ENC_PIN_A) << 1) | digitalRead(RIGHT_ENC_PIN_B);
 
-  Serial.print(prevVal);
+  Serial.print(prevVal,BIN);
   Serial.print("   :   ");
-  Serial.print(val);
+  Serial.print(val,BIN);
   Serial.print("   :   ");
   Serial.println(ENC_STATES[prevVal][val]);
 }
