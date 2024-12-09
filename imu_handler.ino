94314664fb2214beed9e6c90959509e3276b382d
basil <basilkhamala@gmail.com>
2024-12-09 11:21:35 +0300

    small changes

--- imu_handler.ino
+++ imu_handler.ino
@@ -82,26 +82,26 @@ void initIMU() {
 // }
 
 void printFormattedFloat(float val, uint8_t leading, uint8_t decimals) {
-  float aval = abs(val);
-  if (val < 0) {
-    Serial.print("-");
-  } else {
-    Serial.print(" ");
-  }
-  for (uint8_t indi = 0; indi < leading; indi++) {
-    uint32_t tenpow = 0;
-    if (indi < (leading - 1)) {
-      tenpow = 1;
-    }
-    for (uint8_t c = 0; c < (leading - 1 - indi); c++) {
-      tenpow *= 10;
-    }
-    if (aval < tenpow) {
-      Serial.print("0");
-    } else {
-      break;
-    }
-  }
+  // float aval = abs(val);
+  // if (val < 0) {
+  //   Serial.print("-");
+  // } else {
+  //   Serial.print(" ");
+  // }
+  // for (uint8_t indi = 0; indi < leading; indi++) {
+  //   uint32_t tenpow = 0;
+  //   if (indi < (leading - 1)) {
+  //     tenpow = 1;
+  //   }
+  //   for (uint8_t c = 0; c < (leading - 1 - indi); c++) {
+  //     tenpow *= 10;
+  //   }
+  //   if (aval < tenpow) {
+  //     Serial.print("0");
+  //   } else {
+  //     break;
+  //   }
+  // }
   if (val < 0) {
     Serial.print(-val, decimals);
   } else {
